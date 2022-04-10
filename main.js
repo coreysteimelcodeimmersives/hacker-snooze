7c192e263da6f118c535a7c505b23299e0b8de29
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-04-10 16:10:57 -0600

    update comments, more news button

--- main.js
+++ main.js
@@ -10,76 +10,95 @@ let hackerNewsCarouselStoryTwoDOM = document.querySelector("#hackerNewsCarouselS
 let hackerNewsCarouselSubTextTwoDOM = document.querySelector('#hackerNewsCarouselSubTexTwo');
 let hackerNewsCarouselStoryThreeDOM = document.querySelector("#hackerNewsCarouselStoryThree");
 let hackerNewsCarouselSubTextThreeDOM = document.querySelector('#hackerNewsCarouselSubTextThree');
+let hackerNewsCarouselMoreButtonDOM = document.querySelector('#hackerNewsCarouselMoreButton');
 let newCarouselStoryOneDOM = document.querySelector("#newCarouselStoryOne");
 let newCarouselStoryTwoDOM = document.querySelector("#newCarouselStoryTwo");
 let newCarouselStoryThreeDOM = document.querySelector("#newCarouselStoryThree");
+let newCarouselMoreButtonDOM = document.querySelector('#newCarouselMoreButton');
 let pastCarouselStoryOneDOM = document.querySelector("#pastCarouselStoryOne");
 let pastCarouselStoryTwoDOM = document.querySelector("#pastCarouselStoryTwo");
 let pastCarouselStoryThreeDOM = document.querySelector("#pastCarouselStoryThree");
+let pastCarouselMoreButtonDOM = document.querySelector('#pastCarouselMoreButton');
 let askCarouselStoryOneDOM = document.querySelector('#askCarouselStoryOne');
 let askCarouselStoryTwoDOM = document.querySelector("#askCarouselStoryTwo");
 let askCarouselStoryThreeDOM = document.querySelector("#askCarouselStoryThree");
+let askCarouselMoreButtonDOM = document.querySelector('#askCarouselMoreButton');
 let showCarouselStoryOneDOM = document.querySelector("#showCarouselStoryOne");
 let showCarouselStoryTwoDOM = document.querySelector("#showCarouselStoryTwo");
 let showCarouselStoryThreeDOM = document.querySelector("#showCarouselStoryThree");
+let showCarouselMoreButtonDOM = document.querySelector('#showCarouselMoreButton');
 let podiumTitleOneDOM = document.querySelector('#podiumTitleOne');
 let podiumTitleTwoDOM = document.querySelector('#podiumTitleTwo');
 let podiumTitleThreeDOM = document.querySelector('#podiumTitleThree');
 let podiumSubTextOneDOM = document.querySelector('#podiumSubTextOne');
 let podiumSubTextTwoDOM = document.querySelector('#podiumSubTextTwo');
 let podiumSubTextThreeDOM = document.querySelector('#podiumSubTextThree');
+let podiumCommentsNumberOneDOM = document.querySelector('#podiumCommentsNumberOne');
+let podiumCommentsNumberTwoDOM = document.querySelector('#podiumCommentsNumberTwo');
+let podiumCommentsNumberThreeDOM = document.querySelector('#podiumCommentsNumberThree');
 let podiumTitleDOMArray = [podiumTitleOneDOM, podiumTitleTwoDOM, podiumTitleThreeDOM];
 let podiumSubTextDOMArray = [podiumSubTextOneDOM, podiumSubTextTwoDOM, podiumSubTextThreeDOM];
+let podiumCommentsNumberArray = [podiumCommentsNumberOneDOM, podiumCommentsNumberTwoDOM, podiumCommentsNumberThreeDOM];
+let hackerNewsPodiumTitlesArray = ['Hacker News One', 'Hacker News Two', 'Hacker News Three'];
+let hackerNesPodiumSubTextPointsArray = ['77', '201', '18'];
+let hackerNewsPodiumSubTextAuthorArray = ['Johnny Green', 'Billy Bob', 'Buster Brown'];
+let hackerNewsPodiumCommentsNumberArray = ['1', '11', '111'];
+let newPodiumTitlesArray = ['News News One', 'News News Two', 'News News Three'];
+let newPodiumSubTextPointsArray = ['88', '103', '47'];
+let newPodiumSubTextAuthorArray = ['Gary Yellow', 'Kevin Hank', 'Yikes Spikes'];
+let newPodiumCommentsNumberArray = ['2', '22', '222'];
+let pastPodiumTitlesArray = ['Past One', 'Past Two', 'Past Three'];
+let pastPodiumSubTextPointsArray = ['44', '307', '23'];
+let pastPodiumSubTextAuthorArray = ['Britt Dawg', 'Jade Cool', 'Ari MyGirl'];
+let pastPodiumCommentsNumberArray = ['3', '33', '333'];
+let askPodiumTitlesArray = ['Ask One', 'Ask Two', 'Ask Three'];
+let askPodiumSubTextPointsArray = ['11', '698', '71'];
+let askPodiumSubTextAuthorArray = ['Reed Ster', 'Corey James', 'Riskin Nice'];
+let askPodiumCommentsNumberArray = ['4', '44', '444'];
+let showPodiumTitlesArray = ['Show One', 'Show Two', 'Show Three'];
+let showPodiumSubTextPointsArray = ['55', '876', '21'];
+let showPodiumSubTextAuthorArray = ['Shan Dawg', 'Chris Blick', 'Young Eric'];
+let showCommentsNumberArray = ['5', '55', '555'];
 
 // Click Listeners
-hackerNewsNav.addEventListener('click', function(){
-    let podiumTitlesArray = ['Hacker News One', 'Hacker News Two', 'Hacker News Three'];
-    let podiumSubTextPointsArray = ['77', '201', '18'];
-    let podiumSubTextAuthorArray = ['Johnny Green', 'Billy Bob', 'Buster Brown'];
-    for (let i = 0; i < podiumTitleDOMArray.length; i++){
-        setPodiumTitles(podiumTitleDOMArray[i], podiumTitlesArray[i]);
-        setPodiumSubText(podiumSubTextDOMArray[i], podiumSubTextPointsArray[i], podiumSubTextAuthorArray[i]);
-    }
+hackerNewsNav.addEventListener('click', function () {
+    updatePodium(hackerNewsPodiumTitlesArray, hackerNesPodiumSubTextPointsArray, hackerNewsPodiumSubTextAuthorArray, hackerNewsPodiumCommentsNumberArray);
 });
 
-newNav.addEventListener('click', function(){
-    let podiumTitlesArray = ['News News One', 'News News Two', 'News News Three'];
-    let podiumSubTextPointsArray = ['88', '103', '47'];
-    let podiumSubTextAuthorArray = ['Gary Yellow', 'Kevin Hank', 'Yikes Spikes'];
-    for (let i = 0; i < podiumTitleDOMArray.length; i++){
-        setPodiumTitles(podiumTitleDOMArray[i], podiumTitlesArray[i]);
-        setPodiumSubText(podiumSubTextDOMArray[i], podiumSubTextPointsArray[i], podiumSubTextAuthorArray[i]);
-    }
+hackerNewsCarouselMoreButtonDOM.addEventListener('click', function () {
+    updatePodium(hackerNewsPodiumTitlesArray, hackerNesPodiumSubTextPointsArray, hackerNewsPodiumSubTextAuthorArray, hackerNewsPodiumCommentsNumberArray);
+})
+
+newNav.addEventListener('click', function () {
+    updatePodium(newPodiumTitlesArray, newPodiumSubTextPointsArray, newPodiumSubTextAuthorArray, newPodiumCommentsNumberArray);
 });
 
-pastNav.addEventListener('click', function(){
-    let podiumTitlesArray = ['Past One', 'Past Two', 'Past Three'];
-    let podiumSubTextPointsArray = ['44', '307', '23'];
-    let podiumSubTextAuthorArray = ['Britt Dawg', 'Jade Cool', 'Ari MyGirl'];
-    for (let i = 0; i < podiumTitleDOMArray.length; i++){
-        setPodiumTitles(podiumTitleDOMArray[i], podiumTitlesArray[i]);
-        setPodiumSubText(podiumSubTextDOMArray[i], podiumSubTextPointsArray[i], podiumSubTextAuthorArray[i]);
-    }
+newCarouselMoreButtonDOM.addEventListener('click', function(){
+    updatePodium(newPodiumTitlesArray, newPodiumSubTextPointsArray, newPodiumSubTextAuthorArray, newPodiumCommentsNumberArray);
+})
+
+pastNav.addEventListener('click', function () {
+    updatePodium(pastPodiumTitlesArray, pastPodiumSubTextPointsArray, pastPodiumSubTextAuthorArray, pastPodiumCommentsNumberArray);
 });
 
-askNav.addEventListener('click', function(){
-    let podiumTitlesArray = ['Ask One', 'Ask Two', 'Ask Three'];
-    let podiumSubTextPointsArray = ['11', '698', '71'];
-    let podiumSubTextAuthorArray = ['Reed Ster', 'Corey James', 'Riskin Nice'];
-    for (let i = 0; i < podiumTitleDOMArray.length; i++){
-        setPodiumTitles(podiumTitleDOMArray[i], podiumTitlesArray[i]);
-        setPodiumSubText(podiumSubTextDOMArray[i], podiumSubTextPointsArray[i], podiumSubTextAuthorArray[i]);
-    }
+pastCarouselMoreButtonDOM.addEventListener('click', function () {
+    updatePodium(pastPodiumTitlesArray, pastPodiumSubTextPointsArray, pastPodiumSubTextAuthorArray, pastPodiumCommentsNumberArray);
 });
 
-showNav.addEventListener('click', function(){
-    let podiumTitlesArray = ['Show One', 'Show Two', 'Show Three'];
-    let podiumSubTextPointsArray = ['55', '876', '21'];
-    let podiumSubTextAuthorArray = ['Shan Dawg', 'Chris Blick', 'Young Eric'];
-    for (let i = 0; i < podiumTitleDOMArray.length; i++){
-        setPodiumTitles(podiumTitleDOMArray[i], podiumTitlesArray[i]);
-        setPodiumSubText(podiumSubTextDOMArray[i], podiumSubTextPointsArray[i], podiumSubTextAuthorArray[i]);
-    }
+askNav.addEventListener('click', function () {
+    updatePodium(askPodiumTitlesArray, askPodiumSubTextPointsArray, askPodiumSubTextAuthorArray, askPodiumCommentsNumberArray);
+});
+
+askCarouselMoreButtonDOM.addEventListener('click', function () {
+    updatePodium(askPodiumTitlesArray, askPodiumSubTextPointsArray, askPodiumSubTextAuthorArray, askPodiumCommentsNumberArray);
+});
+
+showNav.addEventListener('click', function () {
+    updatePodium(showPodiumTitlesArray, showPodiumSubTextPointsArray, showPodiumSubTextAuthorArray, showCommentsNumberArray);
+});
+
+showCarouselMoreButtonDOM.addEventListener('click', function () {
+    updatePodium(showPodiumTitlesArray, showPodiumSubTextPointsArray, showPodiumSubTextAuthorArray, showCommentsNumberArray);
 });
 
 // Helper Functions
@@ -89,4 +108,16 @@ let setPodiumTitles = (podiumTitleDOM, podiumTitle) => {
 
 let setPodiumSubText = (podiumSubTextDOM, points, author) => {
     podiumSubTextDOM.innerText = `${points} points by ${author}`;
+}
+
+let setPodiumCommentsNumber = (podiumCommentsDOM, commentsNumber) => {
+    podiumCommentsDOM.innerText = `${commentsNumber} Comments »`
+}
+
+let updatePodium = (podiumTitlesArray, podiumSubTextPointsArray, podiumSubTextAuthorArray, podiumCommentsNumber) => {
+    for (let i = 0; i < podiumTitleDOMArray.length; i++) {
+        setPodiumTitles(podiumTitleDOMArray[i], podiumTitlesArray[i]);
+        setPodiumSubText(podiumSubTextDOMArray[i], podiumSubTextPointsArray[i], podiumSubTextAuthorArray[i]);
+        setPodiumCommentsNumber(podiumCommentsNumberArray[i], podiumCommentsNumber[i]);
+    }
 }
